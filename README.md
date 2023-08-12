53b003fe8ea0a0778c95a68a94d147edb26ec849
= <=>
2023-08-12 21:20:56 +0000

    Created basic user input interface.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 declaration.  Otherwise, it prompts for a potentially non-existent password.
 * `justpy.io` and all of its dependencies appear to consume approximately 89M
 of diskspace.
+* I got the flag emojis from [here](https://emojiguide.org/).
 
 ## List of pip packages for learning purposes
 * `addict` allows dictionary assignment like this `my_struct.foo.bar = 'baz'`
--- languages.py
+++ languages.py
@@ -2,6 +2,24 @@ import justpy as jp
 
 levels = ['A1','A2','B1']
 languages = ['🇫🇷','🇨🇳','🇲🇽']
+stories = ['Read','Listen']
+
+def create_radio_btn_group(src_list_name,is_text=True):
+    div = jp.Div(classes='flex justify-center items-center')
+    
+    btn_list = []
+    src_list = globals().get(src_list_name)
+    for item in src_list:
+        label = jp.Label(classes='inline-block mb-1 p-1',a=div)
+        jp.Input(type='radio',name=src_list_name,value=item,a=label,
+        btn_list=btn_list)
+        btn_list.append(item)
+        classes = 'ml-1'
+        if is_text:
+            classes += ' bold text-md'
+        jp.Span(classes='ml-1',a=label,text=item)
+        
+    return div
 
 def index():
     wp = jp.WebPage()
@@ -9,24 +27,21 @@ def index():
     jp.H2(text='Learn A New Language!',classes="text-5xl bold mb-3",a=container)
     
     jp.H2(text="Languages",classes='text-2xl',a=container)
-    div_lang = jp.Div(classes="flex justify-center items-center",a=container)
-    language_list = []
-    for language in languages:
-        label = jp.Label(classes='inline-block mb-1 p-1',a=div_lang)
-        radio_btn = jp.Input(type='radio',name='language',value=language,a=label,
-            btn_list=language_list)
-        language_list.append(language)
-        jp.Span(classes='ml-1',a=label,text=language)
-        
+    container.add(create_radio_btn_group('languages',False))
+
     jp.H2(text="Level",classes='text-2xl',a=container)
-    div_level = jp.Div(classes="flex justify-center items-center",a=container)
-    level_list = []
-    for level in levels:
-        label = jp.Label(classes="inline-block mb-1 p-1",a=div_level)
-        radio_btn = jp.Input(type='radio',name='level',value=level,a=label,
-            btn_list=level_list)
-        level_list.append(level)
-        jp.Span(classes='ml-1 bold text-md',a=label,text=level)
+    container.add(create_radio_btn_group('levels'))
+        
+    jp.H2(text='I want to...',classes='text-2xl',a=container)
+    container.add(create_radio_btn_group('stories'))
+    
+    jp.Input(placeholder = 'Add up to 5 tags to facilitate story creation...',
+    classes='focus:outline-none border border-gray-300 p-1 mr-2 mb-2 w-11/12 \
+        shadow-lg rounded-md',a=container)
+    
+    jp.Button(text='Go',classes='bg-gray-200 p-2 rounded',a=container)
+    
+
     
     return wp
 
