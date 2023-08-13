9e48f688086d0409347232a071c569a7e528dc87
= <=>
2023-08-13 14:34:16 +0000

    Started gathering parameters for story creation.

--- languages.py
+++ languages.py
@@ -1,25 +1,40 @@
 import justpy as jp
 
 levels = ['A1','A2','B1']
-languages = ['🇫🇷','🇨🇳','🇲🇽']
-stories = ['Read','Listen']
+languages = [('🇫🇷','French'),('🇨🇳','Mandarin'),('🇲🇽','Mexican Spanish')]
+media = ['Read','Listen']
+
+story_params = dict.fromkeys(['levels','languages','media','tags'])
+
+def radio_changed(self,msg):
+    for btn in self.btn_list:
+        if btn.checked:
+            story_params[self.name] = btn.value
 
 def create_radio_btn_group(src_list_name,is_text=True):
     div = jp.Div(classes='flex justify-center items-center')
-    
     btn_list = []
     src_list = globals().get(src_list_name)
     for item in src_list:
-        label = jp.Label(classes='inline-block mb-1 p-1',a=div)
-        jp.Input(type='radio',name=src_list_name,value=item,a=label,
-        btn_list=btn_list)
-        btn_list.append(item)
+        val = item
         classes = 'ml-1'
         if is_text:
             classes += ' bold text-md'
+        else:
+            val = item[1]
+        label = jp.Label(classes='inline-block mb-1 p-1',a=div)
+        radio_btn = jp.Input(type='radio',name=src_list_name,value=val,a=label,
+        btn_list=btn_list,change=radio_changed)
+        btn_list.append(radio_btn)
+
         jp.Span(classes='ml-1',a=label,text=item)
         
     return div
+    
+def get_params(self,msg):
+    tags = self.input.value.split(' ')[:5]
+    story_params['tags'] = tags
+    print(story_params)
 
 def index():
     wp = jp.WebPage()
@@ -33,15 +48,17 @@ def index():
     container.add(create_radio_btn_group('levels'))
         
     jp.H2(text='I want to...',classes='text-2xl',a=container)
-    container.add(create_radio_btn_group('stories'))
+    container.add(create_radio_btn_group('media'))
     
-    jp.Input(placeholder = 'Add up to 5 tags to facilitate story creation...',
-    classes='focus:outline-none border border-gray-300 p-1 mr-2 mb-2 w-11/12 \
+    input = jp.Input(placeholder = 'Add up to 5 tags to facilitate story creation...',
+        classes='focus:outline-none border border-gray-300 p-1 mr-2 mb-2 w-11/12 \
         shadow-lg rounded-md',a=container)
     
-    jp.Button(text='Go',classes='bg-gray-200 p-2 rounded',a=container)
+    go_btn = jp.Button(text='Go',classes='bg-gray-200 p-2 rounded mb-4',a=container)
+    go_btn.on('click',get_params)
+    go_btn.input = input
     
-
+    jp.Div(classes="flex justify-right")
     
     return wp
 
